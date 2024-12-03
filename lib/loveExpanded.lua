7e944cfbe74c8e1e2dcba4ef408361b963bddd5a
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2024-12-03 19:51:32 -0300

    i think its done

--- lib/loveExpanded.lua
+++ lib/loveExpanded.lua
@@ -1,19 +1,23 @@
+-- Interpolates a string with the given arguments.
 function string.interpolate(_Str,_Args)
     return (_Str:gsub('($%b{})', function(w) return _Args[w:sub(3, -2)] or w end))
 end
 
+-- Clamps a value between a minimum and maximum value.
 function setLimits(_Min,_Value,_Max)
     if _Value < _Min then return _Min end
     if _Value > _Max then return _Max end
     return _Value
 end
 
+-- Checks if a value is within a specified range.
 function inLimits(_Min,_Value,_Max)
     if _Value < _Min then return false end
     if _Value > _Max then return false end
     return true
 end
 
+-- Joins multiple values into a single string.
 function strJoin(...)
     local args = {...}
     local str = ""
@@ -23,8 +27,8 @@ function strJoin(...)
     return str
 end
 
+-- Draws an image centered at the given coordinates.
 function drawCentered(_Draw, _X, _Y, _R, _Sx , _Sy)
     local width, height = _Draw:getDimensions()
     love.graphics.draw(_Draw, _X, _Y, _R, _Sx, _Sy ,width/2, height/2)
 end
-
--- main.lua
+++ main.lua
@@ -14,7 +14,9 @@ end
 
 function changeGameState(_State,_Arg)
     currentGameState = gameStates[_State]
-    currentGameState.load(_Arg)
+    if currentGameState.load then
+        currentGameState.load(_Arg)
+    end
     love.window.setTitle(_State)
 end
 
@@ -26,31 +28,31 @@ function love.update(dt)
     currentGameState.update(dt)
 end
 
-function love.draw(dt)
-    currentGameState.draw(dt)
+function love.draw()
+    currentGameState.draw()
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
     if currentGameState.onMouseClick then
-        currentGameState.onMouseClick({x=x,y=y,button=button,istouch=istouch,presses=presses})
+        currentGameState.onMouseClick(x, y, button, istouch, presses)
     end
 end
 
 function love.keypressed(key, scancode, isrepeat)
     if currentGameState.onKeyPress then
-        currentGameState.onKeyPress({key=key,scancode=scancode,isrepeat=isrepeat})
+        currentGameState.onKeyPress(key, scancode, isrepeat)
     end
 end
 
 function love.keyreleased(key, scancode)
     if currentGameState.onKeyRelease then
-        currentGameState.onKeyRelease({key=key,scancode=scancode})
+        currentGameState.onKeyRelease(key, scancode)
     end
 end
 
 function love.mousemoved(x, y, dx, dy, istouch)
     if currentGameState.onMouseMove then
-        currentGameState.onMouseMove({x=x,y=y,dx=dx,dy=dy,istouch=istouch})
+        currentGameState.onMouseMove(x, y, dx, dy, istouch)
     end
 end
 
@@ -68,12 +70,12 @@ end
 
 function love.resize(w, h)
     if currentGameState.onResize then
-        currentGameState.onResize({w=w,h=h})
+        currentGameState.onResize(w, h)
     end
 end
 
-
-
-
-
-
+function love.textinput(t)
+    if currentGameState.onTextInput then
+        currentGameState.onTextInput(t)
+    end
+end
